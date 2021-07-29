b34701f2219df232a8584089f7472280647c6d04
Eli <Eli.Ateljevich@water.ca.gov>
2021-07-29 16:59:44 -0700

    Minor fixes to setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ install_requires = ['vtools>=3.4','pandas','geopandas>=0.6',
                     'xarray>=0.15', 'netcdf4>=1.4', 'scipy>=1.4',
                     'matplotlib>=3.1', 'statsmodels>=0.11',
                     'palettable>=3.3', 'pyyaml>=5.3',
-                    'pytest-runner','unittest','sklearn','stats','statsmodels',
+                    'pytest-runner','unittest','scikit-learn','statsmodels',
                     'urllib','beautifulsoup4','pyproj','gdal','nodepy',
                     'shapely','rasterstats>=0.14',
                     'sphinx','nbsphinx','sphinx-argparse','numpydoc']
