7786e0350a1bca014c5c594410ac4a25cca7f31e
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-14 01:45:01 +0800

    Delete schimpy directory

--- schimpy/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Top-level package for schimpy."""
-
-__author__ = """Eli Ateljevich, Kijin Nam"""
-__email__ = 'Eli.Ateljevich@water.ca.gov; Kijin.Nam@water.ca.gov'
-
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
-
