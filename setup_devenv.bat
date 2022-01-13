ba5d85dc55d53c3477a0140f2675d1c4b1a9a992
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-14 01:58:50 +0800

    Delete setup_devenv.bat

--- setup_devenv.bat
+++
@@ -1,6 +0,0 @@
-conda create -y -c defaults -n dev_schimpy python=3.7*
-conda install -y -c defaults -n dev_schimpy rtree numpy gdal xarray matplotlib shapely pyyaml scipy pyproj geopandas
-# Below this for dev environment, etc
-conda install -y -c defaults -n dev_schimpy pytest pytest-runner
-# Now for document generation
-conda install -y -c defaults -c conda-forge -n dev_schimpy sphinx nbsphinx sphinx-argparse numpydoc
