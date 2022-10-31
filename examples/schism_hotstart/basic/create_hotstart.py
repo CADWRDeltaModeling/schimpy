a0116da55b04901a3860c156e138daf06442eaba
Zhang <Zhenlin.Zhang@water.ca.gov>
2022-10-31 14:55:39 -0700

    updating hotstart input files

--- examples/schism_hotstart/basic/create_hotstart.py
+++ examples/schism_hotstart/basic/create_hotstart.py
@@ -8,7 +8,7 @@ Created on Wed Oct 21 11:12:19 2020
 import schimpy.schism_hotstart as sh
 import matplotlib.pyplot as plt
 
-yaml_fn = "./data_in/hotstart.yaml"
+yaml_fn = "./hotstart.yaml"
 modules = ['TEM','SAL']
 hotstart_fn = "hotstart.nc" # output hotstart file
 ini_date = '2021-04-20' 
