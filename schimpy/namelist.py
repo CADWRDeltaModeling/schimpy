b6e368fd469af8ccf139a5410c82c17df155b1a8
Nicky Sandhu <psandhu@water.ca.gov>
2023-02-09 20:07:03 -0800

    namelist now preserves comments better

--- schimpy/namelist.py
+++ schimpy/namelist.py
@@ -1,43 +1,52 @@
 def parse(file_content):
     namelists = {}
     current_namelist = None
-    full_line_comment = None
+    full_line_comment = ""
     for line in file_content.splitlines():
         line = line.strip()
         if line.startswith("!"):
-            full_line_comment = line[1:].strip()
+            if len(full_line_comment) == 0:
+                full_line_comment = line[1:]
+            else:
+                full_line_comment = full_line_comment + '\n' + line[1:]
         elif line.startswith("&"):
             current_namelist = line[1:].strip()
             namelists[current_namelist] = {"comment": full_line_comment}
-            full_line_comment = None
+            full_line_comment = ""
         elif line.startswith("/"):
             current_namelist = None
         elif current_namelist is not None:
             line_parts = line.split("!", maxsplit=1)
             key_value_pair = line_parts[0].strip()
-            inline_comment = line_parts[1].strip() if len(line_parts) == 2 else None
+            inline_comment = line_parts[1] if len(line_parts) == 2 else ""
             if not key_value_pair:
                 continue
             key, value = key_value_pair.split("=")
             key = key.strip()
             value = value.strip()
             namelists[current_namelist][key] = {"value": value, "full_line_comment": full_line_comment, "inline_comment": inline_comment}
-            full_line_comment = None
+            full_line_comment = ""
     return namelists
 
 def write(namelist_data):
     lines = []
     for namelist_name, namelist_values in namelist_data.items():
-        lines.append("&{}".format(namelist_name))
         comment = namelist_values.get("comment")
-        if comment:
-            lines.append("! {}".format(comment))
+        if comment and len(comment) != 0:
+            lines.append("!{}".format(comment.replace("\n","\n!")))
+        lines.append("&{}".format(namelist_name))
         for key, value_data in namelist_values.items():
-            if key == "comment":
-                continue
+            if key == "comment": # already taken care of above
+                continue 
+            if "full_line_comment" in value_data: # do this first
+                full_line_comment = value_data.get("full_line_comment")
+                if len(full_line_comment) != 0:
+                    lines.append("!{}".format(full_line_comment.replace("\n","\n!")))
+            if key == "full_line_comment":
+                continue # took care of it above
             lines.append("  {} = {}".format(key, value_data["value"]))
             inline_comment = value_data.get("inline_comment")
-            if inline_comment:
-                lines.append("  ! {}".format(inline_comment))
+            if inline_comment and len(inline_comment) != 0 :
+                lines[-1] = lines[-1] + (" !{}".format(inline_comment.replace('\n','')))
         lines.append("/")
     return "\n".join(lines)
--- tests/test_namelist.py
+++ tests/test_namelist.py
@@ -14,12 +14,12 @@ def test_namelist_parser():
     namelists = namelist.parse(file_content)
     print(namelists)
 
-    expected_namelists = {'NAMELIST1': {'comment': None,
-            'VAR1': {'value': '1', 'full_line_comment': 'a full line comment', 'inline_comment': 'a line comment'},
-            'VAR2': {'value': '2', 'full_line_comment': None, 'inline_comment': 'another comment'}},
-        'NAMELIST2': {'comment': None,
-            'VAR3': {'value': '3', 'full_line_comment': None, 'inline_comment': None},
-        'VAR4': {'value': '4', 'full_line_comment': None, 'inline_comment': None}}}
+    expected_namelists = {'NAMELIST1': {'comment': "",
+            'VAR1': {'value': '1', 'full_line_comment': ' a full line comment', 'inline_comment': ' a line comment'},
+            'VAR2': {'value': '2', 'full_line_comment': "", 'inline_comment': ' another comment'}},
+        'NAMELIST2': {'comment': "",
+            'VAR3': {'value': '3', 'full_line_comment': "", 'inline_comment': ""},
+        'VAR4': {'value': '4', 'full_line_comment': "", 'inline_comment': ""}}}
 
     assert namelists == expected_namelists
 
@@ -44,7 +44,7 @@ def test_write_namelist():
             }
         }
     }
-    expected_output = "&first_namelist\n! This is the first namelist\n  first_value = 1\n  ! This is the first value\n  second_value = 2\n/\n&second_namelist\n! This is the second namelist\n  third_value = 3\n  ! This is the third value\n/"
+    expected_output = "!This is the first namelist\n&first_namelist\n  first_value = 1 !This is the first value\n  second_value = 2\n/\n!This is the second namelist\n&second_namelist\n  third_value = 3 !This is the third value\n/"
     output = namelist.write(namelist_data)
     assert output == expected_output, f"Expected: {expected_output}\nGot: {output}"
 
@@ -67,3 +67,17 @@ def test_write_namelist():
     output = namelist.write(namelist_data)
     assert output == expected_output, f"Expected: {expected_output}\nGot: {output}"
 
+def test_sample_param_nml():
+    url = 'https://raw.githubusercontent.com/schism-dev/schism/master/sample_inputs/param.nml'
+    import urllib.request
+    with urllib.request.urlopen(url) as response:
+        content = response.read().decode("utf-8")
+    with open('param-original.nml','w') as fh: fh.write(content)
+    from schimpy import namelist
+    params = namelist.parse(content)
+    with open('param.nml','w') as fh: fh.write(namelist.write(params))
+    with open('param.nml','r') as fh:
+        params2 = namelist.parse(fh.read())
+    with open('param2.nml','w') as fh: fh.write(namelist.write(params2))
+    import filecmp
+    assert filecmp.cmp('param.nml','param2.nml'), 'Reading and writing from parser to writer is resulting in differences!'
