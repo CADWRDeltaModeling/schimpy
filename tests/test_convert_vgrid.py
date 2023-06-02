d4ddebfbabcf7241928dc93cc47418fe0c6ac69c
Kijin Nam <knam@water.ca.gov>
2023-06-02 13:08:00 -0700

    Update vgrid test codes
    This commits updates some vgrid test codes to use pytest. Some default
    arguments are fixed as well.
    
    SCH-605

--- tests/test_convert_vgrid.py
+++ tests/test_convert_vgrid.py
@@ -5,23 +5,35 @@ Created on Wed Apr 13 17:21:42 2022
 @author: zzhang
 """
 
+import pathlib
+import pytest
 from schimpy.schism_vertical_mesh import convert_vmesh, compare_vmesh, read_vmesh
 
-old_vgrid_fn = 'testdata/vgrid.in.old'
-new_vgrid_fn = 'testdata/vgrid.in.new'
+@pytest.fixture
+def datadir():
+    return pathlib.Path(__file__).parent.resolve() / 'testdata'
 
-output_grid_fn1 = 'vgrid1.in.3d'
-output_grid_fn2 = 'vgrid2.in.3d'
 
-convert_vmesh(old_vgrid_fn,output_grid_fn1) #converting from old to new vgrid
-convert_vmesh(new_vgrid_fn,output_grid_fn2,input_vgrid=5.9,output_vgrid=5.8) #converting from new to old vgrid.
+def test_convert_vmesh(datadir):
+    old_vgrid_fn = datadir / 'vgrid.in.old'
+    new_vgrid_fn = datadir / 'vgrid.in.new'
 
-# check if they are still identify with the original grid. 
-mesh1 = read_vmesh(old_vgrid_fn,vgrid_version=5.8)
-mesh2 = read_vmesh(output_grid_fn1,vgrid_version=5.9)
-compare_vmesh(mesh1,mesh2)
+    output_grid_fn1 = 'vgrid1.in.3d'
+    output_grid_fn2 = 'vgrid2.in.3d'
+    # converting from old to new vgrid
+    convert_vmesh(old_vgrid_fn, output_grid_fn1)
+    # converting from new to old vgrid.
+    convert_vmesh(new_vgrid_fn, output_grid_fn2,
+                  input_vgrid="5.10", output_vgrid="5.8")
+    # check if they are still identify with the original grid.
+    mesh1 = read_vmesh(old_vgrid_fn, vgrid_version="5.8")
+    mesh2 = read_vmesh(output_grid_fn1, vgrid_version="5.10")
+    compare_vmesh(mesh1, mesh2)
 
-mesh1 = read_vmesh(new_vgrid_fn,vgrid_version=5.9)
-mesh2 = read_vmesh(output_grid_fn2,vgrid_version=5.8)
-compare_vmesh(mesh1,mesh2)
 
+def test_compare_vmesh(datadir):
+    new_vgrid_fn = datadir / 'vgrid.in.new'
+    output_grid_fn2 = 'vgrid2.in.3d'
+    mesh1 = read_vmesh(new_vgrid_fn, vgrid_version="5.10")
+    mesh2 = read_vmesh(output_grid_fn2, vgrid_version="5.8")
+    compare_vmesh(mesh1, mesh2)
