4d4ad89abaa6d06d97f178e226fa47de45743f2e
Kijin Nam <knam@water.ca.gov>
2023-06-06 11:20:40 -0700

    Clean up test_params.py
    The commit adds a routine to use pytest from the package base directory
    and the code is reformatted. No functional changes.

--- tests/test_params.py
+++ tests/test_params.py
@@ -1,16 +1,24 @@
-# tests for params.py 
+# tests for params.py
 # Param NML
+import pathlib
 import pytest
 from schimpy import params, namelist
 
-def test_params():
-    param_file = 'testdata/param.nml'
-    with open(param_file,'r') as fh: param_dict = namelist.parse(fh.read())
+
+@pytest.fixture
+def datadir():
+    return pathlib.Path(__file__).parent.resolve() / 'testdata'
+
+
+def test_params(datadir):
+    param_file = datadir / 'param.nml'
+    with open(param_file, 'r') as fh:
+        param_dict = namelist.parse(fh.read())
     assert 'CORE' in param_dict
     p = params.create_params(param_dict)
     assert p.core.ipre == 0
     p.core.ipre = 1
     assert p.core.ipre == 1
     with pytest.raises(Exception) as ex:
-      p.core.ipre=2
+        p.core.ipre = 2
     assert p.core.ipre == 1
