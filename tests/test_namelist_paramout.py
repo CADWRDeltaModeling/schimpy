9b6894935ea4226caa5c26c2c14e1331007f4d96
Kijin Nam <knam@water.ca.gov>
2023-08-22 15:57:33 -0700

    Update a test to pick up the current dir
    Updates a test code to use the file location to find out a test input
    file.

--- tests/test_namelist_paramout.py
+++ tests/test_namelist_paramout.py
@@ -1,6 +1,10 @@
+from pathlib import Path
 from schimpy import nml
 
+
 def test_read_param_out():
-    with open("testdata/param.out.nml","r") as fin:
+    dirpath = Path(__file__).parent
+    filepath = dirpath / "testdata/param.out.nml"
+    with open(filepath, "r") as fin:
         nmldata = nml.parse(fin.read())
-    assert nmldata['SCHOUT']['IOF_HYDRO']['value'] == '1, 16*0, 2*1, 5*0, 2*1, 14*0,'
+    assert nmldata["SCHOUT"]["IOF_HYDRO"]["value"] == "1, 16*0, 2*1, 5*0, 2*1, 14*0,"
