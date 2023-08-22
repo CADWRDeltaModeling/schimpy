8d49156b6f30af37ce0add848999dc53b1917c88
Kijin Nam <knam@water.ca.gov>
2023-08-21 17:10:13 -0700

    Correct a typo
    Removes an extra `from`.

--- schimpy/plot_default_formats.py
+++ schimpy/plot_default_formats.py
@@ -4,7 +4,7 @@
 
 
 import vtools.data.timeseries
-from from vtools.functions.unit_conversions import m_to_ft, cms_to_cfs,\
+from vtools.functions.unit_conversions import m_to_ft, cms_to_cfs,\
                                     celsius_to_fahrenheit,\
                                     psu_ec_25c, psu_ec_25c_scalar, ec_sea
 import palettable
