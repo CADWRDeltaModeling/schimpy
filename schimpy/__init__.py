8d9623161f37cdb74415f25b501b95124c1eed83
lily-tomkovic-DWR <Lily.Tomkovic@water.ca.gov>
2024-02-20 13:56:59 -0800

    update versioneer

--- schimpy/__init__.py
+++ schimpy/__init__.py
@@ -3,7 +3,6 @@
 __author__ = """Eli Ateljevich, Kijin Nam"""
 __email__ = 'Eli.Ateljevich@water.ca.gov; Kijin.Nam@water.ca.gov'
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
 
