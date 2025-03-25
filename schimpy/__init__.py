d80a158caa0daea51d84a0d36dc895b22305c895
lily-tomkovic-DWR <lily.tomkovic@water.ca.gov>
2025-03-25 14:55:44 -0700

    setup pyproject.toml

--- schimpy/__init__.py
+++ schimpy/__init__.py
@@ -1,8 +1,8 @@
 """Top-level package for schimpy."""
 
 __author__ = """Eli Ateljevich, Kijin Nam"""
-__email__ = 'Eli.Ateljevich@water.ca.gov; Kijin.Nam@water.ca.gov'
+__email__ = "Eli.Ateljevich@water.ca.gov; Kijin.Nam@water.ca.gov"
 
-from . import _version
-__version__ = _version.get_versions()['version']
+from .. import _version
 
+__version__ = _version.get_versions()["version"]
