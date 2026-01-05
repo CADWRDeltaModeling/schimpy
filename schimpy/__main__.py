b3da72430ec8f6d6ad8849cbc18a50055aa94744
lily-tomkovic-DWR <lily.tomkovic@water.ca.gov>
2026-01-05 14:55:32 -0800

    convert small areas to click

--- schimpy/__main__.py
+++ schimpy/__main__.py
@@ -14,8 +14,8 @@ from schimpy.hotstart_inventory import hotstart_inventory_cli
 from schimpy.create_vgrid_lsc2 import create_vgrid_lsc2_cli
 from schimpy.schism_hotstart import create_hotstart_cli
 from schimpy.split_quad import split_quads_cli
+from schimpy.small_areas import small_areas_cli
 
-# from schimpy.small_areas import small_areas
 # from schimpy.station import station
 # from schimpy.nudging import create_nudging
 # from schimpy.interpolate_structure import interpolate_structure
@@ -47,8 +47,8 @@ cli.add_command(hotstart_inventory_cli, "hotstart_inventory")
 cli.add_command(create_vgrid_lsc2_cli, "create_vgrid_lsc2")
 cli.add_command(create_hotstart_cli, "schism_hotstart")
 cli.add_command(split_quads_cli, "split_quad")
+cli.add_command(small_areas_cli, "small_areas")
 
-# cli.add_command(small_areas, "small_areas")
 # cli.add_command(station, "station")
 # cli.add_command(create_nudging, "create_nudging")
 # cli.add_command(interpolate_structure, "interpolate_structure")
--- schimpy/small_areas.py
+++ schimpy/small_areas.py
@@ -2,21 +2,19 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-import argparse
+import click
 from schimpy.schism_mesh import read_mesh
 
 
-def create_arg_parser():
-    """Create ArgumentParser"""
-    parser = argparse.ArgumentParser(description="Identify small areas")
-    parser.add_argument("--input_mesh", type=str, default=None, help="Input mesh")
-    parser.add_argument(
-        "--warn", type=float, default=10.0, help="Threshold for warning (areas smaller)"
-    )
-    parser.add_argument(
-        "--fail", type=float, default=4.0, help="Threshold for failure (areas smaller)"
-    )
-    return parser
+@click.command()
+@click.option('--input_mesh', type=str, default=None, help='Input mesh')
+@click.option('--warn', type=float, default=10.0, help='Threshold for warning (areas smaller)')
+@click.option('--fail', type=float, default=4.0, help='Threshold for failure (areas smaller)')
+def small_areas_cli(input_mesh, warn, fail):
+    """Identify small areas"""
+    print(input_mesh)
+    mesh = read_mesh(input_mesh)
+    small_areas(mesh, warn, fail)
 
 
 def small_areas(
@@ -84,15 +82,5 @@ def small_areas(
     return
 
 
-def main():
-    parser = create_arg_parser()
-    args = parser.parse_args()
-    print(args.input_mesh)
-    mesh = read_mesh(args.input_mesh)
-    warn = args.warn
-    fail = args.fail
-    small_areas(mesh, warn, fail)
-
-
 if __name__ == "__main__":
-    main()
+    small_areas_cli()
