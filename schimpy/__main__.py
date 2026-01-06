3b478bf90934439534b92df111e60ffc2a465ee1
lily-tomkovic-DWR <lily.tomkovic@water.ca.gov>
2026-01-05 17:07:19 -0800

    comment out possibly unused functions

--- schimpy/__main__.py
+++ schimpy/__main__.py
@@ -21,22 +21,24 @@ from schimpy.interpolate_structure import interpolate_structure_cli
 from schimpy.merge_th import merge_th_cli
 from schimpy.create_station_output import create_station_output_cli
 from schimpy.param import set_param_cli
-from schimpy.check_mesh_skewness import check_skewness_cli
-from schimpy.combine_flux import combine_flux_cli
-from schimpy.convert_sav_class_to_number import convert_sav_class_to_number_cli
-from schimpy.create_mesh_n_levels import create_mesh_n_levels_cli
-from schimpy.cruise import cruise_plot_cli
-from schimpy.cut_mesh import cut_mesh_cli
-from schimpy.download_hrrr import download_hrr_clip_cli
-from schimpy.embed_raster import embed_raster_cli
-from schimpy.schism_yaml import schism_yaml_cli
-from schimpy.grid_opt import grid_opt_cli
-from schimpy.material_poly import material_poly_cli
-from schimpy.simulation_timing import simulation_timing_cli
-from schimpy.stacked_dem_fill import stacked_fill_cli
-from schimpy.mesh_volume_tvd import mesh_volume_tvd_cli
-from schimpy.subset_schism_output import subset_schism_output_cli
-from schimpy.scripts.archive_ts import archive_ts_cli
+
+# Commands to be checked for utility before re-adding
+# from schimpy.check_mesh_skewness import check_skewness_cli
+# from schimpy.combine_flux import combine_flux_cli
+# from schimpy.convert_sav_class_to_number import convert_sav_class_to_number_cli
+# from schimpy.create_mesh_n_levels import create_mesh_n_levels_cli
+# from schimpy.cruise import cruise_plot_cli
+# from schimpy.cut_mesh import cut_mesh_cli
+# from schimpy.download_hrrr import download_hrr_clip_cli
+# from schimpy.embed_raster import embed_raster_cli
+# from schimpy.schism_yaml import schism_yaml_cli
+# from schimpy.grid_opt import grid_opt_cli
+# from schimpy.material_poly import material_poly_cli
+# from schimpy.simulation_timing import simulation_timing_cli
+# from schimpy.stacked_dem_fill import stacked_fill_cli
+# from schimpy.mesh_volume_tvd import mesh_volume_tvd_cli
+# from schimpy.subset_schism_output import subset_schism_output_cli
+# from schimpy.scripts.archive_ts import archive_ts_cli
 
 
 @click.group(
@@ -71,21 +73,23 @@ cli.add_command(interpolate_structure_cli, "interpolate_structure")
 cli.add_command(merge_th_cli, "merge_th")
 cli.add_command(create_station_output_cli, "create_station_output")
 cli.add_command(set_param_cli, "set_param")
-cli.add_command(check_skewness_cli, "check_skewness")
-cli.add_command(combine_flux_cli, "combine_flux")
-cli.add_command(convert_sav_class_to_number_cli, "convert_sav_class_to_number")
-cli.add_command(create_mesh_n_levels_cli, "create_mesh_n_levels")
-cli.add_command(cruise_plot_cli, "cruise_plot")
-cli.add_command(cut_mesh_cli, "cut_mesh")
-cli.add_command(download_hrr_clip_cli, "download_hrrr")
-cli.add_command(embed_raster_cli, "embed_raster")
-cli.add_command(schism_yaml_cli, "schism_yaml")
-cli.add_command(grid_opt_cli, "grid_opt")
-cli.add_command(material_poly_cli, "material_poly")
-cli.add_command(simulation_timing_cli, "simulation_timing")
-cli.add_command(mesh_volume_tvd_cli, "mesh_volume_tvd")
-cli.add_command(subset_schism_output_cli, "subset_schism_output")
-cli.add_command(archive_ts_cli, "archive_ts")
+
+# Commands to be checked for utility before re-adding
+# cli.add_command(check_skewness_cli, "check_skewness")
+# cli.add_command(combine_flux_cli, "combine_flux")
+# cli.add_command(convert_sav_class_to_number_cli, "convert_sav_class_to_number")
+# cli.add_command(create_mesh_n_levels_cli, "create_mesh_n_levels")
+# cli.add_command(cruise_plot_cli, "cruise_plot")
+# cli.add_command(cut_mesh_cli, "cut_mesh")
+# cli.add_command(download_hrr_clip_cli, "download_hrrr")
+# cli.add_command(embed_raster_cli, "embed_raster")
+# cli.add_command(schism_yaml_cli, "schism_yaml")
+# cli.add_command(grid_opt_cli, "grid_opt")
+# cli.add_command(material_poly_cli, "material_poly")
+# cli.add_command(simulation_timing_cli, "simulation_timing")
+# cli.add_command(mesh_volume_tvd_cli, "mesh_volume_tvd")
+# cli.add_command(subset_schism_output_cli, "subset_schism_output")
+# cli.add_command(archive_ts_cli, "archive_ts")
 
 if __name__ == "__main__":
     cli()
