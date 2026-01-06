f46ba4d9323f41112ce4a88a53daa12b0cc0587c
lily-tomkovic-DWR <lily.tomkovic@water.ca.gov>
2026-01-05 17:05:12 -0800

    use click for prepare_schism test

--- tests/test_prepare_schism.py
+++ tests/test_prepare_schism.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from schimpy.prepare_schism import prepare_schism
+from schimpy.prepare_schism import prepare_schism_cli
 import unittest
-import os, argparse
+import os
+from click.testing import CliRunner
+
 
 class TestPrepareSchism(unittest.TestCase):
     def setUp(self):
         dir_cur = os.path.dirname(__file__)
         dir_test = os.path.join(dir_cur, "testdata/prepare_schism")
         os.chdir(dir_test)
+        self.runner = CliRunner()
 
     def tearDown(self):
         os.chdir(os.path.dirname(__file__))
 
     def test_prepare_schism(self):
         os.chdir('simple_triquad')
-        args = argparse.Namespace()
-        setattr(args, 'main_inputfile', 'main_good.yaml')
-        prepare_schism(args)
+        result = self.runner.invoke(prepare_schism_cli, ['main_good.yaml'])
+        self.assertEqual(result.exit_code, 0, f"Command failed with output: {result.output}")
 
 
 if __name__ == '__main__':
