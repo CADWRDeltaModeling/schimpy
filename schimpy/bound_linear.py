e16cbc8e18b47ecf95835d7f49975945330035e2
Eli <Eli.Ateljevich@water.ca.gov>
2026-06-24 11:18:06 -0700

    Modified bound_linear to allow ramping with depth in either direction.

--- schimpy/bound_linear.py
+++ schimpy/bound_linear.py
@@ -1,8 +1,9 @@
 import numpy as np
 
-
 def bound_linear(z, z_low, z_high, val_low, val_high):
     z0 = val_low + (val_high - val_low) * (z - z_low) / (z_high - z_low)
-    z0 = np.maximum(z0, val_low)
-    z0 = np.minimum(z0, val_high)
-    return z0
+
+    vmin = min(val_low, val_high)
+    vmax = max(val_low, val_high)
+
+    return np.clip(z0, vmin, vmax)
